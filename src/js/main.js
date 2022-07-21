6a56f7b95473dffb80ef3c8e767550802900906a
Demid Popravko <demid1498@gmail.com>
2022-07-21 15:31:10 +0300

    Tabs module

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import "./slider";
 import modals from './modules/modals';
+import tabs from './modules/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
 })
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,37 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),
+          tab = document.querySelectorAll(tabSelector),
+          content = document.querySelectorAll(contentSelector);
+
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tab.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    function showTabContent(i = 0) {
+        content[i].style.display = 'block';
+        tab[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.classList.contains(tabSelector.replace(/\./, "")) || target.parentNode.classList.contains(tabSelector.replace(/\./, ""))) {
+            tab.forEach((tab, i) => {
+                if (target == tab || target.parentNode == tab) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    })
+};
+
+export default tabs;
